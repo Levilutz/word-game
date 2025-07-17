3a9b19b5f2c877c149fb865c137d39f4e5910ded
Levi Lutz <levi6900@icloud.com>
2025-07-16 21:28:50 -0400

    Update to V4 SearchableWords model
    Restructures column ordering to be alphabet-centric
    Introduces CountAtLeast columns and query
    Removes Contains cols and query (identical to CountAtLeast 1)

--- word_core/src/word_search.rs
+++ word_core/src/word_search.rs
@@ -3,14 +3,14 @@ use crate::word::Word;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Query {
-    /// Filter for words that contain any instances of `chr`
-    Contains { chr: u8 },
-
     /// Filter for words that contain an instance of `chr` at the specified `ind`
     Match { ind: usize, chr: u8 },
 
     /// Filter for words that contain exactly `count` instances of `chr`
     CountExact { count: usize, chr: u8 },
+
+    /// Filter for words that contain at least `count` instances of `chr`
+    CountAtLeast { count: usize, chr: u8 },
 
     /// Filter for words that do not satisfy the child query
     Not(Box<Query>),
@@ -30,55 +30,72 @@ pub struct SearchableWords<const WORD_SIZE: usize, const ALPHABET_SIZE: u8> {
 impl<const WORD_SIZE: usize, const ALPHABET_SIZE: u8> SearchableWords<WORD_SIZE, ALPHABET_SIZE> {
     /// Given a set of words and an alphabet size, build a search table of word data.
     pub fn build(words: Vec<Word<WORD_SIZE>>) -> Self {
-        let num_cols = (ALPHABET_SIZE as usize) * WORD_SIZE * 2;
+        let num_cols = (ALPHABET_SIZE as usize) * WORD_SIZE * 3;
         let mut columns = Vec::with_capacity(num_cols);
 
-        // Push exact match columns
-        for ind in 0..WORD_SIZE {
-            for chr in 0..ALPHABET_SIZE {
-                columns.push(Self::build_match_col(&words, ind, chr))
-            }
-        }
-
-        // Push count columns
+        // Push precomputed columns for each char
         for chr in 0..ALPHABET_SIZE {
+            // Push exact match columns
+            let match_cols = (0..WORD_SIZE).map(|ind| {
+                Column::from_bools(
+                    &words
+                        .iter()
+                        .map(|word| word.0[ind] == chr)
+                        .collect::<Vec<bool>>(),
+                )
+            });
+            columns.extend(match_cols);
+
+            // Compute counts for this character once
             let counts: Vec<u64> = words
                 .iter()
                 .map(|word| word.count_chr(chr) as u64)
                 .collect();
-            let one_hot_cols = Column::one_hot_values(&counts, WORD_SIZE as u64 + 1);
-            columns.extend(one_hot_cols.into_iter());
+
+            // Push count-exact columns
+            let exact_count_cols = Column::one_hot_values(&counts, WORD_SIZE as u64 + 1);
+            columns.extend(exact_count_cols.into_iter());
+
+            // Push count-at-least columns
+            for threshold_count in 1..WORD_SIZE {
+                let word_count_at_least_threshold: Vec<bool> = counts
+                    .iter()
+                    .map(|word_count| *word_count >= threshold_count as u64)
+                    .collect();
+                let count_at_least_col = Column::from_bools(&word_count_at_least_threshold);
+                columns.push(count_at_least_col);
+            }
         }
 
         Self { words, columns }
     }
 
-    /// Build a column representing whether a given character is present in a given ind.
-    fn build_match_col(words: &[Word<WORD_SIZE>], ind: usize, chr: u8) -> Column {
-        Column::from_bools(
-            &words
-                .iter()
-                .map(|word| word.0[ind] == chr)
-                .collect::<Vec<bool>>(),
-        )
-    }
-
     /// Evaluate the query and produce an output column that represents a mask over rows.
     pub fn eval_query(&self, query: Query) -> Column {
         match query {
-            Query::Contains { chr } => self.eval_query(Query::Not(Box::new(Query::CountExact {
-                count: 0,
-                chr: chr,
-            }))),
             Query::Match { ind, chr } => {
-                let target_col = ALPHABET_SIZE as usize * ind + chr as usize;
+                let chr_block_start = (WORD_SIZE * 3) * chr as usize;
+                let chr_block_match_cols_start = chr_block_start + 0;
+                let target_col = chr_block_match_cols_start + ind;
                 self.columns[target_col].clone()
             }
             Query::CountExact { count, chr } => {
-                let target_col = (ALPHABET_SIZE as usize * WORD_SIZE as usize)
-                    + ((WORD_SIZE as usize + 1) * chr as usize)
-                    + count;
+                let chr_block_start = (WORD_SIZE * 3) * chr as usize;
+                let chr_block_count_exact_cols_start = chr_block_start + WORD_SIZE;
+                let target_col = chr_block_count_exact_cols_start + count;
                 self.columns[target_col].clone()
+            }
+            Query::CountAtLeast { count, chr } => {
+                if count == 0 {
+                    Column::from_true(self.words.len())
+                } else if count == WORD_SIZE {
+                    self.eval_query(Query::CountExact { count, chr })
+                } else {
+                    let chr_block_start = (WORD_SIZE * 3) * chr as usize;
+                    let chr_block_count_at_least_cols_start = chr_block_start + WORD_SIZE * 2 + 1;
+                    let target_col = chr_block_count_at_least_cols_start + count - 1;
+                    self.columns[target_col].clone()
+                }
             }
             Query::Not(query) => !self.eval_query(*query),
             Query::And(queries) => {
@@ -169,15 +186,6 @@ mod tests {
     }
 
     #[test]
-    fn test_query_contains() {
-        assert_query_result_and_inverse::<3, 26>(
-            &["foo", "bar", "baf"],
-            Query::Contains { chr: 5 },
-            &["foo", "baf"],
-        );
-    }
-
-    #[test]
     fn test_query_match() {
         assert_query_result_and_inverse::<3, 26>(
             &["foo", "bar", "baz"],
@@ -187,7 +195,7 @@ mod tests {
     }
 
     #[test]
-    fn test_query_count() {
+    fn test_query_count_exact() {
         assert_query_result_and_inverse::<3, 26>(
             &["bbc", "cbc", "abc", "bca", "baa", "aac", "aaa"],
             Query::CountExact { count: 0, chr: 0 },
@@ -211,12 +219,36 @@ mod tests {
     }
 
     #[test]
+    fn test_query_count_at_least() {
+        assert_query_result_and_inverse::<3, 26>(
+            &["bbc", "cbc", "abc", "bca", "baa", "aac", "aaa"],
+            Query::CountAtLeast { count: 0, chr: 0 },
+            &["bbc", "cbc", "abc", "bca", "baa", "aac", "aaa"],
+        );
+        assert_query_result_and_inverse::<3, 26>(
+            &["bbc", "cbc", "abc", "bca", "baa", "aac", "aaa"],
+            Query::CountAtLeast { count: 1, chr: 0 },
+            &["abc", "bca", "baa", "aac", "aaa"],
+        );
+        assert_query_result_and_inverse::<3, 26>(
+            &["bbc", "cbc", "abc", "bca", "baa", "aac", "aaa"],
+            Query::CountAtLeast { count: 2, chr: 0 },
+            &["baa", "aac", "aaa"],
+        );
+        assert_query_result_and_inverse::<3, 26>(
+            &["bbc", "cbc", "abc", "bca", "baa", "aac", "aaa"],
+            Query::CountAtLeast { count: 3, chr: 0 },
+            &["aaa"],
+        );
+    }
+
+    #[test]
     fn test_query_and_group() {
         assert_query_result_and_inverse::<3, 26>(
             &["foo", "bar", "baz", "biz", "buz"],
             Query::And(vec![
                 Query::Match { ind: 1, chr: 0 },
-                Query::Contains { chr: 25 },
+                Query::CountAtLeast { count: 1, chr: 25 },
             ]),
             &["baz"],
         );
@@ -228,7 +260,7 @@ mod tests {
             &["foo", "bar", "baz", "biz", "buz"],
             Query::Or(vec![
                 Query::Match { ind: 1, chr: 0 },
-                Query::Contains { chr: 25 },
+                Query::CountAtLeast { count: 1, chr: 25 },
             ]),
             &["bar", "baz", "biz", "buz"],
         );
@@ -250,10 +282,10 @@ mod tests {
                 // The D was correct
                 Query::Match { ind: 4, chr: 3 },
                 // There is no O
-                Query::Not(Box::new(Query::Contains { chr: 14 })),
+                Query::Not(Box::new(Query::CountAtLeast { count: 1, chr: 14 })),
                 // The A was elsewhere
                 Query::And(vec![
-                    Query::Contains { chr: 0 },
+                    Query::CountAtLeast { count: 1, chr: 0 },
                     Query::Not(Box::new(Query::Match { ind: 2, chr: 0 })),
                 ]),
                 // The R was elsewhere (alternate representation)

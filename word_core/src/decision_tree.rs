65af51dd8507e6cd1818df7fef5ce04256ebbb5c
Levi Lutz <levi6900@icloud.com>
2025-07-19 15:21:15 -0400

    Fix tree branch estimated cost calculation

--- word_core/src/decision_tree.rs
+++ word_core/src/decision_tree.rs
@@ -41,6 +41,9 @@ pub fn compute_node_aggressive<const WORD_SIZE: usize, const ALPHABET_SIZE: u8>(
         if !do_print && depth == 0 {
             println!("evaluating top level guess {}", guess);
         }
+        if do_print {
+            println!("{}> evaluating guess {}", prefix, guess)
+        }
         let child_allowed_guesses: Vec<Word<WORD_SIZE>> = allowed_guesses
             .iter()
             .filter(|allowed_guess| *allowed_guess != guess)
@@ -48,7 +51,7 @@ pub fn compute_node_aggressive<const WORD_SIZE: usize, const ALPHABET_SIZE: u8>(
             .collect();
         let mut guess_decision_tree: HashMap<WordHint<WORD_SIZE>, TreeNode<WORD_SIZE>> =
             HashMap::new();
-        let mut guess_avg_perf = 0.0;
+        let mut guess_est_cost = 1.0;
         for word_hint in WordHint::all_possible() {
             let mask = possible_answers.eval_query(clue_to_query(*guess, word_hint));
             let num_answers_giving_this_hint = mask.count_true();
@@ -57,12 +60,12 @@ pub fn compute_node_aggressive<const WORD_SIZE: usize, const ALPHABET_SIZE: u8>(
             }
             if num_answers_giving_this_hint == possible_answers.len() as u64 {
                 // This guess doesn't filter at all and is worthless, don't consider
-                guess_avg_perf = INFINITY;
+                guess_est_cost = INFINITY;
                 break;
             }
             if do_print {
                 println!(
-                    "{}{} could come from {} answers - {}",
+                    "{}scenario {} could come from {} answers - {}",
                     prefix,
                     word_hint.color_guess(guess),
                     num_answers_giving_this_hint,
@@ -74,7 +77,19 @@ pub fn compute_node_aggressive<const WORD_SIZE: usize, const ALPHABET_SIZE: u8>(
                         .join(", ")
                 );
             }
-            let (child_node, child_est_guesses) = match num_answers_giving_this_hint {
+            if word_hint.all_correct() {
+                // We happened to guess correctly, there is no additional cost
+                if do_print {
+                    println!(
+                        "{}scenario {} est addl cost {}",
+                        prefix,
+                        word_hint.color_guess(guess),
+                        0,
+                    );
+                }
+                continue;
+            }
+            let (child_node, child_est_addl_cost) = match num_answers_giving_this_hint {
                 1 => (
                     TreeNode::Answer {
                         answer: possible_answers.filter_words(&mask)[0],
@@ -88,30 +103,35 @@ pub fn compute_node_aggressive<const WORD_SIZE: usize, const ALPHABET_SIZE: u8>(
                     do_print,
                 ),
             };
-            guess_avg_perf += (child_est_guesses + 1.0) * num_answers_giving_this_hint as f64
+            if do_print {
+                println!(
+                    "{}scenario {} est addl cost {}",
+                    prefix,
+                    word_hint.color_guess(guess),
+                    child_est_addl_cost,
+                );
+            }
+            guess_est_cost += child_est_addl_cost * num_answers_giving_this_hint as f64
                 / possible_answers.len() as f64;
             guess_decision_tree.insert(word_hint, child_node);
         }
-        if guess_avg_perf == INFINITY {
+        if do_print {
+            println!("{}> guess {} est cost {}", prefix, guess, guess_est_cost);
+        }
+        if guess_est_cost == INFINITY {
             continue;
         }
-        if do_print {
-            println!(
-                "{}> guess {} has avg perf of {}",
-                prefix, guess, guess_avg_perf
-            );
-        }
         match best {
-            Some((_, _, best_guess_avg_perf)) if best_guess_avg_perf <= guess_avg_perf => {
+            Some((_, _, best_guess_est_cost)) if best_guess_est_cost <= guess_est_cost => {
                 continue;
             }
-            _ => best = Some((*guess, guess_decision_tree, guess_avg_perf)),
+            _ => best = Some((*guess, guess_decision_tree, guess_est_cost)),
         }
     }
     let (best_guess, best_guess_decision_tree, best_guess_avg_perf) = best.unwrap();
     if do_print {
         println!(
-            "{}>> recommending {} with avg perf of {}",
+            "{}>> recommending {} with est cost of {}",
             prefix, best_guess, best_guess_avg_perf
         );
     }
